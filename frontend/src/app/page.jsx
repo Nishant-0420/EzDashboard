b261e5baab8e94db7c67dfefcb98abb676d4dfe7
NISHANT <thakurnishant0420@gmail.com>
2024-03-14 18:37:06 +0530

    building ezdashboard documentation site

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -20,7 +20,7 @@ export default function Home() {
         <Container size={640}>
           <Text size="lg" className={classes.description}>
             Build more reliable software with AI companion. AI is also trained to detect lazy
-            developers who do nothing and just complain on Twitter.
+            developers who do nothing and just complain on X.
           </Text>
         </Container>
 
