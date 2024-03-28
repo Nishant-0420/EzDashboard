1c3ccfba5cc89df535c37ff05235f1d15dd662de
Vaibhav0049 <kumar.vaibhav2004@bbdu.ac.in>
2024-03-28 17:52:32 +0530

    aadding header navbar

--- frontend/src/app/doc/layout.jsx
+++ frontend/src/app/doc/layout.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar from './navbar';
 import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import Header from './header';
 
 const MainLayout = ({ children }) => {
 
@@ -16,9 +17,10 @@ const MainLayout = ({ children }) => {
                 header={{ height: 60 }}
                 navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
                 padding="md"
+                // layout='alt'
             >
                 <AppShell.Header>
-
+                        <Header />
                 </AppShell.Header>
                 <AppShell.Navbar>
                     <Navbar />
